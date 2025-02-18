1f5fd7550ac7807bfc8e95c744ab28e1b4396aa5
Jhonattan <jhonattanlino@outlook.com>
2025-02-18 12:20:11 -0300

    feat: fix connection bug

--- src/rag.py
+++ src/rag.py
@@ -1,19 +1,28 @@
-import os
-from langchain_openai import AzureChatOpenAI
+########################### Langchain ###########################
+#from openai import AzureOpenAI
+from langchain_openai import AzureOpenAI
+import os  
+import base64
+from openai import AzureOpenAI  
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider  
+        
+endpoint = os.getenv("ENDPOINT_URL", "https://jhona-m7aj9hc3-swedencentral.openai.azure.com/")  
+deployment = os.getenv("DEPLOYMENT_NAME", "gpt-35-turbo")  
+      
+# Inicializar o cliente do Serviço OpenAI do Azure com autenticação do Entra ID
+token_provider = get_bearer_token_provider(  
+    DefaultAzureCredential(),  
+    "https://cognitiveservices.azure.com/.default"  
+)  
+  
+client = AzureOpenAI(  
+    azure_endpoint=endpoint,  
+    azure_ad_token_provider=token_provider,  
+    api_version="2024-05-01-preview",  
+)  
 
-endpoint = os.getenv("ENDPOINT_URL", "https://oai-cvm358.openai.azure.com/")
-deployment = os.getenv("DEPLOYMENT_NAME", "gpt-4o-mini")
-subscription_key = os.getenv("AZURE_OPENAI_API_KEY", "15b50eb85377452b841989f625b8f577")
-
-# Inicializar o cliente do Serviço OpenAI do Azure com autenticação baseada em chave
-client = AzureChatOpenAI(
-    deployment_name=deployment,
-    api_key=subscription_key,
-    api_version="2024-05-01-preview",
-    azure_endpoint=endpoint
+llm = AzureOpenAI(
+    deployment_name = 'gpt-35-turbo',
 )
-
-# Exemplo de uso
-response = client.invoke("Olá, como você está?")
-print(response)
-
+llm.invoke("Tell me a joke")
+print(llm)
