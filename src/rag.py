a99521210f970b18dc8bfdd1c0cef6fc2f9e270c
Jhonattan <jhonattanlino@outlook.com>
2025-02-18 12:18:57 -0300

    feat: connection lanchain with azure open ai

---
+++ src/rag.py
@@ -0,0 +1,19 @@
+import os
+from langchain_openai import AzureChatOpenAI
+
+endpoint = os.getenv("ENDPOINT_URL", "https://oai-cvm358.openai.azure.com/")
+deployment = os.getenv("DEPLOYMENT_NAME", "gpt-4o-mini")
+subscription_key = os.getenv("AZURE_OPENAI_API_KEY", "15b50eb85377452b841989f625b8f577")
+
+# Inicializar o cliente do Serviço OpenAI do Azure com autenticação baseada em chave
+client = AzureChatOpenAI(
+    deployment_name=deployment,
+    api_key=subscription_key,
+    api_version="2024-05-01-preview",
+    azure_endpoint=endpoint
+)
+
+# Exemplo de uso
+response = client.invoke("Olá, como você está?")
+print(response)
+
