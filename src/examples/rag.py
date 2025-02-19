532ce9ea2c5b24aff64d9ba6c0a4aec99ceee5f1
Jhonattan <jhonattanlino@outlook.com>
2025-02-19 17:56:08 -0300

    feat: read pdf file with docling

---
+++ src/examples/rag.py
@@ -0,0 +1,28 @@
+########################### Langchain ###########################
+import os
+from dotenv import load_dotenv
+from langchain_openai import AzureChatOpenAI
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_docling.loader import DoclingLoader
+from langchain_text_splitters import PyTextSplitter
+
+
+load_dotenv()
+
+endpoint = os.getenv("ENDPOINT_URL")
+deployment = os.getenv("DEPLOYMENT_NAME")
+subscription_key = os.getenv("AZURE_OPENAI_API_KEY")
+
+# Inicializar o cliente do Serviço OpenAI do Azure com autenticação baseada em chave
+client = AzureChatOpenAI(
+    azure_deployment=deployment,
+    api_key=subscription_key, # type: ignore
+    api_version="2024-05-01-preview",
+    azure_endpoint=endpoint
+)
+
+# Load a PDF document
+load = DoclingLoader('2311.04727v2.pdf')
+
+data = load.load()
+print(data[0])
