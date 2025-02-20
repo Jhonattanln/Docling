e4158cc294e4da23ce73101fff7b371d88a53847
Jhonattan <jhonattanlino@outlook.com>
2025-02-20 14:42:25 -0300

    feat: last version

--- src/examples/rag.py
+++ src/examples/rag.py
@@ -2,9 +2,11 @@
 import os
 from dotenv import load_dotenv
 from langchain_openai import AzureChatOpenAI
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
 from langchain_docling.loader import DoclingLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 
@@ -27,9 +29,46 @@ data = load.load()
 
 # Split o pdf
 rc_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=500,
-    chunk_overlap=200,
-    separators=["\n", "\n\n"]
+    chunk_size=1000, # número de caracteres por chunk
+    chunk_overlap=200, # número de caracteres de sobreposição
+    separators=["\n", "\n\n"] # separadores de chunk
 )
+
 docs = rc_splitter.split_documents(data)
-print(docs[0])
+
+# Embedding dos chunks
+embed_function = OpenAIEmbeddings(api_key=subscription_key, model='text-embedding-3-small')
+
+vectorstore = Chroma(
+    collection_name="docs",
+    embedding_function=embed_function,
+    persist_directory='data/rag'
+)
+
+retriever = vectorstore.as_retriever(
+    search_type = 'similarity',
+    search_kwargs = {'k': 5}
+)
+
+# Query
+query = """
+Answer the following question using the context provided:
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:
+"""
+
+prompt_template = PromptTemplate([("human", query)])
+
+rag_chain = ({'context': retriever, 'question': RunnablePassthrough()}
+            | prompt_template
+            | client)
+
+response = rag_chain.invoke('What is the conclusion of the paper?')
+print(response.content)
+
